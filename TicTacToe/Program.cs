e1d64e333649de3910675258dd2b4a53bec853a9
solaiman <sulibaraka5@gmail.com>
2025-03-24 15:52:28 +0100

    VERSION BETA 1

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace TicTacToe;
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
 
-class Program
+namespace TicTacToe;
+
+static class Program
 {
-    static void Main(string[] args)
+    private const string Url = "http://localhost:8080/";
+    private const string UrlJugadors = Url + "jugadors/";
+    private const string UrlPartida = Url + "partida/";
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var jugadors = await PullInfo();
+        if (jugadors is null) throw new Exception("No hi ha jugadors a la llist, null exception");
+        var paisJugadors = ConverteixPresentacionsEnPaisJugadors(jugadors);
+    }
+    static async Task<List<string>?> PullInfo()
+    {
+        List<String>? contentList;
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(UrlJugadors);
+            var content = await response.Content.ReadAsStringAsync();
+            contentList = JsonSerializer.Deserialize<List<string>>(content);
+        }
+        return contentList;
+    }
+    static Dictionary<String, String> ConverteixPresentacionsEnPaisJugadors(List<string> presentacions)
+    {
+        const string patronJugador = @"participant\s([a-zA-Z'-]+\s[a-zA-Z'-]+)";
+        const string patronPais = @"representa?[a-zA-Z]+\s[a-zA-Z]+\s([a-zA-Z]+)";
+        
+        Regex rgxJugador = new Regex(patronJugador);
+        Regex rgxPais = new Regex(patronPais);
+        
+        var dictionary = new Dictionary<String, String>();
+        presentacions.ForEach(x =>
+        {
+            var matchJugador = rgxJugador.Match(x);
+            var matchPais = rgxPais.Match(x);
+            
+            var strJugador = matchJugador.Groups[1].Value;
+            var strPais = matchPais.Groups[1].Value;
+            dictionary.Add(strPais, strJugador);
+            
+            Console.WriteLine($"Jugador: {strJugador}, País: {strPais}");
+        });
+        
+        return dictionary;
     }
 }
